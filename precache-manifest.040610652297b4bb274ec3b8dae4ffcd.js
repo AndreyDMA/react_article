0c42d5c430b486fdc2c909c5ffaa9e549b028704
Andrii Dmytriiev <andriidmytriev@gmail.com>
2020-05-12 18:06:01 +0300

    Updates

---
+++ precache-manifest.040610652297b4bb274ec3b8dae4ffcd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "09ab30b68cb70c0dd4822309ec236781",
+    "url": "./index.html"
+  },
+  {
+    "revision": "e2946b4f46f1b5f8612d",
+    "url": "./static/js/2.2671b005.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.2671b005.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "11c503a85945d5b6a8be",
+    "url": "./static/js/main.32c6dbba.chunk.js"
+  },
+  {
+    "revision": "f7084f4ac3b217f5a585",
+    "url": "./static/js/runtime-main.43ba4f59.js"
+  }
+]);
